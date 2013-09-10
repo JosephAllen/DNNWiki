286621b48e965a0cc70ab230ed88c6114aeabb1a
JosephAllen <joseph_m_allen@msn.com>
2013-09-09 20:21:35 -0400

    Added Shared Controls

---
+++ Views/SharedControls/Index.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNetNuke.Modules.Wiki.Views.SharedControls
+{
+    public partial class Index : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
